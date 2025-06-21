505afebc5d921cfbf48bd519aa0813eb3cdc0633
GREYsaurus <greysaurus@gmail.com>
2025-06-21 10:24:10 -0400

    initial commit

---
+++ SimpleTextFormatter/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleTextFormatter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input a string to format:");
+        string input = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("No input provided.");
+            return;
+        }
+        var uppercaseInput = input.ToUpper();
+        var reversedInput = input.Reverse();
+        var replacedInput = input.Replace(" ", "_");
+
+        Console.WriteLine($"Your input was : {input}");
+        Console.WriteLine($"Uppercase: {uppercaseInput}");
+        Console.WriteLine($"Reversed: {new string(reversedInput.ToArray())}");
+        Console.WriteLine($"Replaced spaces with underscores: {replacedInput}");
+        Console.WriteLine("Press any key to exit...");
+    }
+}
